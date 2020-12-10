83a5024e13e9f641acadbc95e355d15be977d84a
clebersonsiqueira <cleberson.siqueira17@gmail.com>
2020-12-10 14:52:08 -0300

    Project Created

---
+++ Aplicacao_Kolping/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aplicacao_Kolping.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
